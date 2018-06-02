f7158fc68b5f905862e0a916db1325332c87dbd1
EC2 Default User <ec2-user@ip-172-31-26-134.us-east-2.compute.internal>
2018-06-02 12:56:24 +0000

    add users#index and users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all.page(params[:page])
+  end
+  
   def new
     @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = 'ユーザーを登録しました'
-      redirect_to root_path
+      redirect_to @user
     else
       flash[:danger] = 'ユーザーの登録に失敗しました'
       render :new
