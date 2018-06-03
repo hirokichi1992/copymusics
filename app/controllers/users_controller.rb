c934b17271818e5e2f6681ad68aefa753ff880ed
EC2 Default User <ec2-user@ip-172-31-26-134.us-east-2.compute.internal>
2018-06-03 10:03:55 +0000

    add follow function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
   
   def index
     @users = User.all.page(params[:page])
@@ -23,7 +24,20 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
   private
+  
   def user_params
     params.require(:user).permit(:name, :email, :age, :part, :password, :password_confirmation)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,13 @@ post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
 
 get 'signup', to: 'users#new'
-resources :users
+
+resources :users do
+  member do
+    get :followings
+    get :followers
+  end
+end
+
+resources :relationships, only: [:create, :destroy]
 end
